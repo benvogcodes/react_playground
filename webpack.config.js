0fec4cd4ee971e5e6fb1990d2fea17b488608dbf
benvogcodes <benjamin.voglewede@gmail.com>
2016-08-25 10:40:49 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,30 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './index.js',
+  context: __dirname + '/src',
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: './js/client.js',
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+        }
+      }
+    ]
+  },
   output: {
-    filename: './bundle.js',
-    path: __dirname
-  }
+    path: __dirname + "/src/",
+    filename: "client.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyPlugin({ mangle: false, sourcemap: false })
+  ]
 };
